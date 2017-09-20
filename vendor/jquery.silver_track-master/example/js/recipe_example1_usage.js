761d26b33b8f10c6cc3289a7a5a8de844f1d0878
Rastislav Kobac <rastislav.kobac@gmail.com>
2017-09-20 16:18:51 +0200

    Deleted files

--- vendor/jquery.silver_track-master/example/js/recipe_example1_usage.js
+++
@@ -1,7 +0,0 @@
-jQuery(function() {
-
-  $(".track.example-1 .slider-container").each(function() {
-    $.silverTrackRecipes.create("basic", $(this)).start();
-  });
-
-});
--- vendor/jquery.silver_track-master/spec/server.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-require 'sinatra'
-
-get '/echo/json/:page' do
-  content_type :json
-  headers("Access-Control-Allow-Origin" => "*")
-
-  { total_pages: params[:total_pages] || 1, data: JSON.parse(params[:json] || "[]") }.to_json
-end
